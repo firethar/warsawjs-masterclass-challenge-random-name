f48b4760ba43810dd93a65b1ccd99dce33af628e
Wojciech Blazejewski <Wojciech.Blazejewski@egplusww.com>
2019-04-04 11:05:42 +0200

    fix: clear non-used hook

--- src/app/random-name/random-name.component.ts
+++ src/app/random-name/random-name.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RandomNameService } from './random-name.service';
 import { Observable} from 'rxjs';
 import { Name } from './name';
@@ -8,16 +8,10 @@ import { Name } from './name';
   templateUrl: './random-name.component.html',
   styleUrls: ['./random-name.component.css']
 })
-export class RandomNameComponent implements OnInit {
+export class RandomNameComponent {
   public errorMessage: string = '';
   public $generatedName: Observable<Name> = this.randomNameService.getRandomName();
 
   constructor(private randomNameService: RandomNameService) {}
 
-  ngOnInit() {
-  }
-
-  // reload() {
-  //   // not used yet
-  // }
 }
